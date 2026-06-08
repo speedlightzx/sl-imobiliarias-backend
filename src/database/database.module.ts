d55ee3b79c3c268453a7cd740d8271bdc5792b5a
speedlightzx <speedlightbr@gmail.com>
2026-06-08 10:45:47 -0300

    feat: adiciona banco e entidades

---
+++ src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Global, Module, Provider } from '@nestjs/common';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg'
+import * as schema from './schemas/schemas'
+
+const databaseProvider: Provider = {
+    provide: 'db',
+    useFactory: () => {
+        const client = new Pool({
+            connectionString: process.env.DB_URL!
+        })
+
+        return drizzle(client, { schema })
+    }
+}
+
+@Global()
+@Module({
+    providers: [databaseProvider],
+    exports: [databaseProvider]
+})
+export class DatabaseModule {}
---
+++ src/database/schemas/schemas.ts
@@ -0,0 +1,31 @@
+import { varchar } from "drizzle-orm/pg-core";
+import { integer } from "drizzle-orm/pg-core";
+import { serial } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const users = pgTable('users', {
+    id: serial('id').primaryKey(),
+    email: varchar('email', { length: 100 }).notNull(),
+    password: varchar('password', { length: 100 }).notNull()
+})
+
+
+export const lists = pgTable('lists', {
+    id: serial('id').primaryKey(),
+    user_id: integer('user_id')
+    .references(() => users.id, {
+        onDelete: 'cascade'
+    })
+    .notNull(),
+    name: varchar('name', { length: 30 }).notNull(),
+    color: varchar('color', { length: 10 })
+})
+
+export const leads = pgTable('leads', {
+    id: serial('id').primaryKey(),
+    list_id: integer('list_id')
+    .references(() => lists.id, {
+        onDelete: 'cascade'
+    })
+    .notNull()
+})
