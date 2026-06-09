0806a4c6939aa89da909fc739e947ce238d4abf6
Allan <speedlightbr@gmail.com>
2026-06-09 12:57:51 +0000

    feat: adiciona guard de autenticacao

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private readonly jwtService:JwtService
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest()
+    const token = req.cookies.session_token
+    if(!token) throw new UnauthorizedException('Token ausente.')
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token)
+
+      req.user = payload
+    } catch(e) {
+      throw new UnauthorizedException("Token inválido.")
+    }
+
+    return true;
+  }
+}
